2ecef027180cdea1da01412674abb9972dae36f5
voytez <vinwap@gmail.com>
2024-02-08 01:24:43 +0400

    343

--- src/diffusers/pipelines/stable_diffusion/safety_checker.py
+++ src/diffusers/pipelines/stable_diffusion/safety_checker.py
@@ -68,7 +68,7 @@ class StableDiffusionSafetyChecker(PreTrainedModel):
                 concept_cos = special_cos_dist[i][concept_idx]
                 concept_threshold = self.special_care_embeds_weights[concept_idx].item()
                 result_img["special_scores"][concept_idx] = round(concept_cos - concept_threshold + adjustment, 3)
-                if -0.1 + result_img["special_scores"][concept_idx] > 0:
+                if -0.01 + result_img["special_scores"][concept_idx] > 0:
                     result_img["special_care"].append({concept_idx, result_img["special_scores"][concept_idx]})
                     adjustment = 0.01
 
@@ -76,7 +76,7 @@ class StableDiffusionSafetyChecker(PreTrainedModel):
                 concept_cos = cos_dist[i][concept_idx]
                 concept_threshold = self.concept_embeds_weights[concept_idx].item()
                 result_img["concept_scores"][concept_idx] = round(concept_cos - concept_threshold + adjustment, 3)
-                if -0.1 + result_img["concept_scores"][concept_idx] > 0:
+                if -0.01 + result_img["concept_scores"][concept_idx] > 0:
                     result_img["bad_concepts"].append(concept_idx)
 
             result.append(result_img)
